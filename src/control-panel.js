6a980c877a5f36f2a58f328efb4063755a1bac58
George <georgeserbanescu@hotmail.com>
2017-03-10 22:36:40 +0200

    Implement actions and action creators

--- src/control-panel.js
+++ src/control-panel.js
@@ -2,15 +2,32 @@ import { Dispatcher , Store} from './flux';
 
 const controlPanelDispatcher = new Dispatcher();
 
+const UPDATE_USERNAME = 'UPDATE_USERNAME';
+const UPDATE_FONT_SIZE_PREFERENCE = 'UPDATE_FONT_SIZE_PREFERENCE';
+
+const userNameUpdateAction = (name) => {
+  return {
+    type: UPDATE_USERNAME,
+    value: name
+  }
+}
+
+const fontSizePreferenceUpdateAction = (size) => {
+  return {
+    type: UPDATE_FONT_SIZE_PREFERENCE,
+    value: size
+  }
+};
+
 document.getElementById('userNameInput').addEventListener('input', ({target}) => {
   const name = target.value;
   console.log('Dispatching ....', name);
-  controlPanelDispatcher.dispatch('TODO__NAMEINPUTACTION');
+  controlPanelDispatcher.dispatch(userNameUpdateAction(name));
 });
 
 document.forms.fontSizeForm.fontSize.forEach(element => {
   element.addEventListener('change', ({target}) => {
-    controlPanelDispatcher.dispatch('TODO__FONTUPDATEACTION');
+    controlPanelDispatcher.dispatch(fontSizePreferenceUpdateAction(target.value));
   })
 })
 
@@ -23,8 +40,16 @@ class UserPrefsStore extends Store {
   }
 
   __onDispatch(action) {
-    console.log('Store received dispatch', action);
-    this.__emitChange();
+    switch(action.type) {
+      case UPDATE_USERNAME:
+        this.__state.userName = action.value;
+        this.__emitChange();
+        break;
+      case UPDATE_FONT_SIZE_PREFERENCE:
+        this.__state.fontSize = action.value;
+        this.__emitChange();
+        break;
+    }
   }
   
   getUserPreferences() {
@@ -36,7 +61,13 @@ const userPrefsStore = new UserPrefsStore(controlPanelDispatcher);
 
 userPrefsStore.addListener((state) => {
   console.info('The current state is ...', state);
+  render(state);
 })
+
+const render = ({userName, fontSize}) => {
+  document.getElementById('userName').innerText = userName;
+  document.getElementsByClassName('container')[0].style.fontSize = fontSize === 'small' ? '16px' : '24px';
+}
 
 controlPanelDispatcher.register(action => {
   console.info('Received action...', action);
